6879018ae2c3c48da92144633d53e7b80a5e8674
Johan Svanström <svanis71@gmail.com>
2018-07-10 21:45:50 +0200

    Healing

--- eventimage.py
+++ eventimage.py
@@ -2,17 +2,14 @@
 
 from flask import Flask, request, make_response
 from flask_restful import Resource, Api
+from template import svg_tmpl
 
 app = Flask(__name__)
 api = Api(app)
 
 class EventImage(Resource):
     def get(self):
-        print('Hej')
-        print(request.args)
         (date, time) = request.args.get('dt').split(' ')
-        with open('circle_tmpl.svg', 'r') as f:
-            svg_tmpl=f.read()
         svg = svg_tmpl % (time, date)
 
         resp = make_response(svg, 201)
